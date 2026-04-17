2fa243d94370d55f45a94944b7fdbd4be340fd22
Fravey <francis.h.cloutier@hotmail.ca>
2026-04-16 22:38:34 -0400

    Test answered - Testing Q4

--- Question 1/Question_1.cs
+++ Question 1/Question_1.cs
@@ -1,16 +1,25 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 public class Program
 {
     public static void Main()
     {
-        List<Item> items = new List<Item>();
-	
+
         //items contains all the items to buy
+        List<Item> items = new List<Item>();
+
         //add the table, paddle and balls with the required quantities
+        items.Add(new Table());
+        items.AddRange(Enumerable.Repeat(new Paddle(), 2));
+        items.AddRange(Enumerable.Repeat(new Balls(), 3));
+
         //Display the price of each item in items
-	
+        foreach (Item item in items)
+        {
+            Console.WriteLine(item.ToString());
+        }
+
         Console.ReadLine();
     }
 }
@@ -20,5 +29,45 @@ public abstract class Item
     protected int price;
     protected float weight;
     abstract public float getFullPrice();
-    public float Weight { get {return weight;} }
+    public float Weight { get { return weight; } }
+
+    public override string ToString()
+    {
+        return $"{this.GetType().Name}: {getFullPrice().ToString("c2")}$";
+    }
+}
+
+public class TaxableItem : Item
+{
+    public override float getFullPrice()
+    {
+        return 1.2f * price;
+    }
+}
+
+public class Table : TaxableItem
+{
+    public Table(int price = 600, float weight = 90)
+    {
+        this.price = price;
+        this.weight = weight;
+    }
+}
+
+public class Paddle : TaxableItem
+{
+    public Paddle(int price = 13, float weight = .2f)
+    {
+        this.price = price;
+        this.weight = weight;
+    }
+}
+
+public class Balls : TaxableItem
+{
+    public Balls(int price = 8, float weight = .06f)
+    {
+        this.price = price;
+        this.weight = weight;
+    }
 }
--- Question 2/Question_2.ps1
+++ Question 2/Question_2.ps1
@@ -1,5 +1,34 @@
 # If you don't have access to powershell on your machine, please do not hesitate to fetch the docker image to do this exercice.
 # The code below is an example on how to fetch the json from the endpoint.
 # Please provide the proper code to have a display that matches the question.
-$response1 = Invoke-WebRequest -URI http://jsonplaceholder.typicode.com/albums
-$response2 = Invoke-WebRequest -URI http://jsonplaceholder.typicode.com/photos
+$albums = Invoke-WebRequest -URI http://jsonplaceholder.typicode.com/albums | ConvertFrom-Json 
+$photos = Invoke-WebRequest -URI http://jsonplaceholder.typicode.com/photos | ConvertFrom-Json 
+
+$photosPerAlbum = $photos | 
+Group-Object -Property albumId -noElement |
+Sort-Object -Property @{Expression = "Count"; Descending = $true}, @{Expression = {$_.Name -as [int]}; Descending = $false}
+
+$topAlbums = $photosPerAlbum | Select-Object -First 5
+
+$hashAlbums=@{}
+foreach ($album in $albums) {
+  $hashAlbums[[int]$album.id] = $album
+}
+
+class Album {
+    [int]$AlbumId
+    [string]$AlbumTitle
+    [int]$PhotoCount;
+}
+
+$albumList = @()
+
+$topAlbums | foreach {
+  $other = $hashAlbums[[int]$_.Name]
+
+  $albumList += [Album]@{AlbumId=$_.Name
+                   AlbumTitle=$other.title
+                  PhotoCount=$_.Count}
+}
+
+Write-Output $albumList
